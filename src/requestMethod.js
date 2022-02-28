f040c184b2c412dc0d091daa7084f4324028f629
MacBooK Air <macbookair@MacBooKs-MacBook-Air.local>
2022-02-28 21:10:00 +0100

    api server added url

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api/";
+const BASE_URL = "https://wistore-server.herokuapp.com/api/";
 let token = "";
 if (!localStorage.getItem("persist:root")) {
   token = "";
